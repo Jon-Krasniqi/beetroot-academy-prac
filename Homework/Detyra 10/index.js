de3097ad2c9cb67f4607fab750a4f7dbddbd0194
Jon Krasniqi <jonnaimkrasniqi@gmail.com>
2021-10-15 21:25:14 +0200

    Finished lesson 25 hw

---
+++ Homework/Detyra 10/index.js
@@ -0,0 +1,32 @@
+let x = 5;
+let y = 2;
+let z = x + y;
+console.log(z);
+
+let u = 5;
+let p = 2;
+let k = u * p;
+console.log(k);
+
+let text1 = "pese ";
+text1 += 5;
+console.log(text1);
+
+let numer = 6;
+console.log(numer ** 6);
+
+let minus = "pese";
+minus -= 4;
+console.log(minus);
+
+let barazim = 6;
+console.log(barazim === 7);
+console.log(barazim !== 7);
+
+console.log(Math.PI ** 2 + 0.016);
+
+console.log(Math.round(6.5));
+console.log(Math.trunc(7.3));
+console.log(Math.sign(-4));
+console.log(Math.random(1 - 11111));
+console.log(Math.cbrt(3));
